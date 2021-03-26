04f835f53091a6a9b15b4a9e6ba1ea0f4a14ece6
%RandomUser% <68478332+MapleHackersoff@users.noreply.github.com>
2021-03-26 22:28:35 +0200

    added qrun lol
    added qrun 4m mapleEutemuin

---
+++ qrun.bat
@@ -0,0 +1,11 @@
+@echo off
+color a
+echo @ running db.html
+start OpenDiscordHaxxUI\dashboard.html
+echo sccs
+cls
+echo @ running odh.exe
+start OpenDiscordHaxx\bin\Debug\OpenDHBackend.exe
+echo sccs
+cls
+exit
